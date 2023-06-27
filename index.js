f904733b493d84a1d3df835dc96131e1e3c98c40
admin <6983299@qq.com>
2023-06-27 09:27:31 +0800

    log

--- index.js
+++ index.js
@@ -76,6 +76,17 @@ app.use(cors({
     origin: ['http://localhost:3000','http://127.0.0.1:5174','http://127.0.0.1:5173', 'https://caomeio.web.app']
 }));
 
+
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+    log_file.write(util.format(d) + '\n');
+    log_stdout.write(util.format(d) + '\n');
+};
+
+
 initializeApp({
   credential: cert(serviceAccount)
 });
