b27a3ced46d0b2aef42f99b7460859a59b3f49c5
admin <6983299@qq.com>
2023-06-27 09:37:19 +0800

    log-restore

--- index.js
+++ index.js
@@ -77,16 +77,6 @@ app.use(cors({
 }));
 
 
-var util = require('util');
-var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
-var log_stdout = process.stdout;
-
-console.log = function(d) { //
-    log_file.write(util.format(d) + '\n');
-    log_stdout.write(util.format(d) + '\n');
-};
-
-
 initializeApp({
   credential: cert(serviceAccount)
 });
